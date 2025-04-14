bd54ffcb30de1622676f70e234212b84627c1018
Mohamad Adil <adilansari8727@gmail.com>
2025-04-14 17:18:16 +0530

    '14-04-25'

--- src/app/components/home/OurTestimonials.jsx
+++ src/app/components/home/OurTestimonials.jsx
@@ -1,6 +1,9 @@
 'use client';
 
-import Testimonials from "../common/Testimonials";
+import Image from 'next/image';
+import Link from 'next/link';
+import { GiRoundStar } from 'react-icons/gi';
+import Testimonials from '../common/Testimonials';
 
 const OurTestimonials = () => {
   return (
@@ -10,10 +13,31 @@ const OurTestimonials = () => {
           <span className="hd-border-line center">Our Testimonials</span>
           <h2>AI Customer Testimonials</h2>
         </div>
+
         <div className="reviews-wrap">
-          {/* Testimonials content will go here */}
+          <div className="avatars">
+            <Image src="/images/testimonials-img/img-1.png" alt="User 1" width={55} height={55} />
+            <Image src="/images/testimonials-img/img-2.png" alt="User 2" width={55} height={55} />
+            <Image src="/images/testimonials-img/img-3.png" alt="User 3" width={55} height={55} />
+            <Image src="/images/testimonials-img/img-4.png" alt="User 4" width={55} height={55} />
+          </div>
+
+          <div className="rating-info">
+            <div className='d-flex align-items-center gap-2'>
+              <div className="ra-star">
+                <GiRoundStar size={20} />
+                <GiRoundStar size={20} />
+                <GiRoundStar size={20} />
+                <GiRoundStar size={20} />
+                <GiRoundStar size={20} />
+              </div>
+              <span className="rating-value">(4.8/5)</span>
+            </div>
+            <span className="reviews">Based on 1,258 reviews</span>
+          </div>
         </div>
-        <Testimonials/>
+
+        <Testimonials />
       </div>
     </section>
   );
